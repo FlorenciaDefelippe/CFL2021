d96d7d410dcfa883b4707b023a1c791ebc59da68
FlorenciaDefelippe <flordefelippe14@gmail.com>
2021-04-24 20:00:55 -0300

    Ejercicios Clase 3

---
+++ Modulo 1 Programación básica/Clase 3/calculoDeDescuento.js
@@ -0,0 +1,8 @@
+// Entrada
+let precioProducto = 450.5;
+let porcentajeDescuento = 0.1;
+// Proceso
+let descuento = precioProducto * porcentajeDescuento;
+let precioFinal = precioProducto - descuento;
+// Salida
+console.log(precioFinal);
